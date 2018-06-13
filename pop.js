41328720df8806938645f6e0f5bcf1390faeb2ce
Dea Brunner <deabrunner@gmail.com>
2018-06-13 18:24:40 +0200

    black knappar

--- pop.js
+++ pop.js
@@ -3,7 +3,6 @@ if (choice != "choice=yes") {
 swal("Denna sida innehåller information om alkoholhaltiga drycker, Bekräfta att du är 20 år eller äldre.",
 {
 buttons: true,
-dangerMode: true,
 })
 .then((value) => {
 	if (value == null) {
