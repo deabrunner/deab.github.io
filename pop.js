883986c994fbe64ffcb647406ac2af1f3f8efe02
Dea Brunner <deabrunner@gmail.com>
2018-06-13 16:15:14 +0200

    add popup
    add popup, cookies etc

---
+++ pop.js
@@ -0,0 +1,17 @@
+var choice = document.cookie
+if (choice != "choice=yes") {
+swal("Denna sida innehåller information om alkoholhaltiga drycker, Bekräfta att du är 20 år eller äldre.",
+{
+buttons: true,
+dangerMode: true,
+})
+.then((value) => {
+	if (value == null) {
+		window.location.replace("http://google.com");}
+	else {
+		document.cookie = "choice=yes"	
+	}	
+	
+	swal(`The returned value is: ${value}`);
+
+})};
